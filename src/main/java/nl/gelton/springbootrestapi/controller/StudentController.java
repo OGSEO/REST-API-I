41da719e8aaaffeb5b32e2e9a697758bc5bfc562
Ollie Gelton <olliegelton@gmail.com>
2024-10-28 20:06:32 +0100

    Working with PathVariable for GetStudentById

--- src/main/java/nl/gelton/springbootrestapi/controller/StudentController.java
+++ src/main/java/nl/gelton/springbootrestapi/controller/StudentController.java
@@ -2,7 +2,11 @@ package nl.gelton.springbootrestapi.controller;
 
 import nl.gelton.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -15,5 +19,22 @@ public class StudentController {
                 "Gelton"
         );
         return student;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Ollie", "Gelton"));
+        students.add(new Student(2, "Manolo", "Gelton"));
+        students.add(new Student(3, "Sharona", "Thonhauser"));
+        students.add(new Student(4, "Santino", "Hendriks"));
+        return students;
+    }
+
+    // {id} - URI template variable
+
+    @GetMapping("students/{id}")
+    public Student getStudentById(@PathVariable("id") int studentId) {
+        return new Student(studentId, "Ollie", "Gelton");
     }
 }
