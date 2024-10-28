e051a08f201bda1bd35a6a51f2240600d6217cc6
Ollie Gelton <olliegelton@gmail.com>
2024-10-28 19:43:47 +0100

    Added HelloWorldController (Warm-Up)

---
+++ src/main/java/nl/gelton/springbootrestapi/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package nl.gelton.springbootrestapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String HelloWorld() {
+        return "Hello World";
+    }
+
+}
