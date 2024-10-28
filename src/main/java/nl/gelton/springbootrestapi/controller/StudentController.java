1ebd34265ecc6175d4500bd441fc22e6a81b246b
Ollie Gelton <olliegelton@gmail.com>
2024-10-28 20:21:32 +0100

    Using @RequestParam for querying

--- src/main/java/nl/gelton/springbootrestapi/controller/StudentController.java
+++ src/main/java/nl/gelton/springbootrestapi/controller/StudentController.java
@@ -3,6 +3,7 @@ package nl.gelton.springbootrestapi.controller;
 import nl.gelton.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -38,5 +39,14 @@ public class StudentController {
                                   @PathVariable("first-name") String firstName,
                                   @PathVariable("last-name") String lastName) {
         return new Student(studentId, firstName, lastName);
+    }
+
+    // Spring boot REST API with request Param
+    // http://localhost:8080/students/query?id=1&firstName=Oldert
+
+    @GetMapping("/students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName) {
+        return new Student(id, firstName, "Gelton");
     }
 }
